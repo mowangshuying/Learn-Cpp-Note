1162ac59c74ba788751c4080ee585124eae4d131
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2022-07-17 09:40:48 +0800

    directory-structure/main.cpp 补充注释内容

--- MWSY-LearnCpp/directory-structure/main.cpp
+++ MWSY-LearnCpp/directory-structure/main.cpp
@@ -1,4 +1,4 @@
-#include <filesystem>
+#include <filesystem> // c++17 开始引入
 #include <string>
 #include <iostream>
 
@@ -11,15 +11,19 @@ void printDirStructure(const std::filesystem::path& path,int level)
 	}
 
 	const std::string spacer(level * 2, ' ');
+	// 通过函数is_regular_file判断是否是文件
 	if (std::filesystem::is_regular_file(path))
 	{
 		std::cout << std::format("{}File: {} ({} bytes)", spacer, path.string(), std::filesystem::file_size(path))<<std::endl;
 		return;
 	}
 
+	// 通过函数is_directory判断是否为目录
 	if (std::filesystem::is_directory(path))
 	{
 		std::cout << std::format("{}Dir: {}", spacer, path.string()) << std::endl;
+		
+		// 遍历该目录下的所有文件
 		for (auto& entry : std::filesystem::directory_iterator(path))
 		{
 			printDirStructure(entry, level + 1);
