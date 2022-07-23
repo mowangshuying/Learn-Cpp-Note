16fd390236124448b2d01d88d15b2a917f98f127
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2022-07-23 08:22:14 +0800

    pair：模板参数推导

--- MWSY-LearnCpp/pair/main.cpp
+++ MWSY-LearnCpp/pair/main.cpp
@@ -5,7 +5,8 @@
 int main(int argc, char** argv)
 {
 	std::pair<int, std::string> p1{ 16,"hello-world!" };
-	//std::pair p2{ true,0.123 };
+	std::pair p2{ true,0.123 };
 	std::cout << std::format("p1=({},{})", p1.first, p1.second) << std::endl;
+	std::cout << std::format("p2=({},{})", p2.first, p2.second) << std::endl;
 	return 0;
 }
