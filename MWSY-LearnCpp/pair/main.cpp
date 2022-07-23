6140dcd002b46fb2fba203bb60258b6fc2018d68
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2022-07-23 08:19:19 +0800

    pair：用于保存两个值（类型自己指定）

---
+++ MWSY-LearnCpp/pair/main.cpp
@@ -0,0 +1,11 @@
+#include <utility>
+#include <iostream>
+#include <string>
+#include <format>
+int main(int argc, char** argv)
+{
+	std::pair<int, std::string> p1{ 16,"hello-world!" };
+	//std::pair p2{ true,0.123 };
+	std::cout << std::format("p1=({},{})", p1.first, p1.second) << std::endl;
+	return 0;
+}
