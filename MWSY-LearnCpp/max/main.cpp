432c31bc5bb462b2ec92c25c5caaa49c15f07194
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2022-07-25 07:30:49 +0800

    最大值和最小值算法

---
+++ MWSY-LearnCpp/max/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <algorithm>
+
+int main(int argc, char** argv)
+{
+	std::cout << std::max(1, 2) << std::endl;
+	return 0;
+}
---
+++ MWSY-LearnCpp/min/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <algorithm>
+
+int main(int argc, char** argv)
+{
+	std::cout << std::min(1, 2) << std::endl;
+	return 0;
+}
