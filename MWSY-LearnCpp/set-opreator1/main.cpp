c7562305ff20209cf0cecdab4bd6e36bb4303fcc
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2022-07-24 22:19:59 +0800

    集合操作并:补充相应注释及注意部分 std::set_union

--- MWSY-LearnCpp/set-opreator1/main.cpp
+++ MWSY-LearnCpp/set-opreator1/main.cpp
@@ -1,5 +1,5 @@
 /**
- * set::set_union集合的并， 
+ * set::set_union集合的并. 
 */
 
 #include <vector>
@@ -21,8 +21,10 @@ int main()
 		for (const auto& i : dest1) {
 			std::cout << i << ' ';
 		}
-		std::cout << '\n';
+		std::cout << '\n'; //输出结果 1 2 3 4 5 6 7
 	}
+
+	//多个元素都会被嵌入
 	{
 		std::vector<int> v1 = { 1, 2, 3, 4, 5, 5, 5 };
 		std::vector<int> v2 = { 3, 4, 5, 6, 7 };
@@ -35,6 +37,24 @@ int main()
 		for (const auto& i : dest1) {
 			std::cout << i << ' ';
 		}
-		std::cout << '\n';
+		std::cout << '\n'; // 输出结果 1 2 3 4 5 5 5 6 7
+	}
+
+	{
+		std::vector<int> v1 = { 1, 2, 4, 3, 5, 5, 5 };
+		std::vector<int> v2 = { 3, 4, 5, 6, 7 };
+		std::vector<int> dest1;
+
+		//v1和v2不是有顺序的的话，不能直接调用set::union，否则触发中断机制
+		std::sort(v1.begin(), v1.end());
+
+		std::set_union(v1.begin(), v1.end(),
+			v2.begin(), v2.end(),
+			std::back_inserter(dest1));
+
+		for (const auto& i : dest1) {
+			std::cout << i << ' ';
+		}
+		std::cout << '\n'; // 输出结果 1 2 3 4 5 5 5 6 7
 	}
 }
